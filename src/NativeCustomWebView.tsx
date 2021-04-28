5c279810ef76c3b02c6d0433cde0638a91657450
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2021-04-28 12:09:38 +0200

    Initial version

--- src/NativeCustomWebView.tsx
+++ src/NativeCustomWebView.tsx
@@ -1,18 +1,145 @@
 import { Component, ReactNode, createElement } from "react";
-import { TextStyle, ViewStyle } from "react-native";
+import { View, TextStyle, ViewStyle } from "react-native";
+import { WebView as RNWebView, WebViewNavigation } from "react-native-webview";
+import { ActionValue } from "mendix";
 
-import { Style } from "@mendix/pluggable-widgets-tools";
+import { mergeNativeStyles, Style } from "@mendix/pluggable-widgets-tools";
 
-import { HelloWorld } from "./components/HelloWorld";
 import { NativeCustomWebViewProps } from "../typings/NativeCustomWebViewProps";
 
 export interface CustomStyle extends Style {
     container: ViewStyle;
-    label: TextStyle;
 }
 
-export class NativeCustomWebView extends Component<NativeCustomWebViewProps<CustomStyle>> {
+interface WebViewStyle extends Style {
+    container: ViewStyle;
+    errorContainer: ViewStyle;
+    errorText: TextStyle;
+}
+
+interface NativeCustomWebViewState {
+    currentUrl: string;
+}
+
+export class NativeCustomWebView extends Component<NativeCustomWebViewProps<CustomStyle>, NativeCustomWebViewState> {
+    state: NativeCustomWebViewState = {
+        currentUrl: ""
+    };
+
+    private readonly onLoadHandler = this.onLoad.bind(this);
+    private readonly onErrorHandler = this.onError.bind(this);
+    private readonly onCallbackHandler = this.onCallback.bind(this);
+    private readonly onNavigationStateChangeHandler = this.onNavigationStateChange.bind(this);
+
+    private webview: RNWebView | null = null;
+    private shouldRender = true;
+    private defaultWebViewStyle: WebViewStyle = {
+        container: {
+            flex: 1,
+            height: "100%",
+            minHeight: 300
+        },
+        errorContainer: {},
+        errorText: {
+            color: "red",
+            fontWeight: "bold"
+        }
+    };
+
     render(): ReactNode {
-        return <HelloWorld name={this.props.yourName} style={this.props.style} />;
+        const uri = this.props.url.value;
+        const styles = mergeNativeStyles(this.defaultWebViewStyle, this.props.style);
+        const containernode =
+            this.shouldRender && uri ? (
+                <View style={styles.container}>
+                    <RNWebView
+                        ref={ref => (this.webview = ref)}
+                        testID={this.props.name}
+                        source={{ uri: uri! }}
+                        style={{
+                            width: "100%",
+                            height: "100%"
+                        }}
+                        onLoadEnd={this.onLoadHandler}
+                        onError={this.onErrorHandler}
+                        onNavigationStateChange={this.onNavigationStateChangeHandler}
+                        sharedCookiesEnabled /* voor iOS */
+                    />
+                </View>
+            ) : (
+                <View />
+            );
+        return containernode;
     }
+
+    componentDidUpdate(prevProps: NativeCustomWebViewProps<CustomStyle>): void {
+        if (!prevProps.url) {
+            this.setState({
+                currentUrl: this.props.url && this.props.url.value ? this.props.url.value : ""
+            });
+        }
+    }
+
+    private onLoad(): void {
+        if (this.props.regexOnLoadInclude && this.props.regexOnLoadInclude.value) {
+            const url: string = (this.state.currentUrl ? this.state.currentUrl : "") as string;
+            if (new RegExp(this.props.regexOnLoadInclude.value).test(url)) {
+                this.executeAction(this.props.onLoad);
+            }
+        } else {
+            this.executeAction(this.props.onLoad);
+        }
+    }
+
+    private onError(): void {
+        this.executeAction(this.props.onError);
+    }
+
+    private onCallback(): void {
+        this.executeAction(this.props.onCallback);
+    }
+
+    private onNavigationStateChange(newNavState: WebViewNavigation): void {
+        // newNavState looks something like this:
+        // {
+        //   url?: string;
+        //   title?: string;
+        //   loading?: boolean;
+        //   canGoBack?: boolean;
+        //   canGoForward?: boolean;
+        // }
+
+        const { url } = newNavState;
+        this.setState({
+            currentUrl: url
+        });
+
+        // console.info("URL", url);
+        let includes = true;
+        let excludes = false;
+        if (this.props.regexInclude && this.props.regexInclude.value) {
+            includes = new RegExp(this.props.regexInclude.value).test(url);
+        }
+        if (this.props.regexExclude && this.props.regexExclude.value) {
+            excludes = new RegExp(this.props.regexExclude.value).test(url);
+        }
+        // console.info("includes, excludes:", includes, excludes);
+        if (includes && !excludes) {
+            this.props.callback.setTextValue(url);
+            this.onCallbackHandler();
+            if (this.webview) {
+                // console.info("loading stopped");
+                if (this.webview) {
+                    this.webview.stopLoading();
+                }
+                this.shouldRender = false;
+            }
+        }
+    }
+
+    private executeAction = (action?: ActionValue): void => {
+        if (action && action.canExecute && !action.isExecuting) {
+            action.execute();
+        }
+    };
 }
--- typings/NativeCustomWebViewProps.d.ts
+++ typings/NativeCustomWebViewProps.d.ts
@@ -3,14 +3,30 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix UI Content Team
  */
+import { ActionValue, DynamicValue, EditableValue } from "mendix";
+
 export interface NativeCustomWebViewProps<Style> {
     name: string;
     style: Style[];
-    yourName: string;
+    url: DynamicValue<string>;
+    regexInclude?: DynamicValue<string>;
+    regexExclude?: DynamicValue<string>;
+    regexOnLoadInclude?: DynamicValue<string>;
+    callback: EditableValue<string>;
+    onLoad?: ActionValue;
+    onError?: ActionValue;
+    onCallback?: ActionValue;
 }
 
 export interface NativeCustomWebViewPreviewProps {
     class: string;
     style: string;
-    yourName: string;
+    url: string;
+    regexInclude: string;
+    regexExclude: string;
+    regexOnLoadInclude: string;
+    callback: string;
+    onLoad: {} | null;
+    onError: {} | null;
+    onCallback: {} | null;
 }
