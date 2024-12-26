72e80506a45d7bede9c53a2ea70c254f5658ca4e
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2024-12-26 09:20:06 +0100

    Mendix 10.18

--- typings/NativeCustomWebViewProps.d.ts
+++ typings/NativeCustomWebViewProps.d.ts
@@ -30,7 +30,8 @@ export interface NativeCustomWebViewPreviewProps {
     style: string;
     styleObject?: CSSProperties;
     readOnly: boolean;
-    renderMode?: "design" | "xray" | "structure";
+    renderMode: "design" | "xray" | "structure";
+    translate: (text: string) => string;
     url: string;
     incognito: boolean;
     cacheEnabled: boolean;
